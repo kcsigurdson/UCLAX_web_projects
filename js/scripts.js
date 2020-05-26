83e12cfe59960b92e7443f9e102c7acd0beacd92
kcsigurdson <katiecsigurdson@gmail.com>
2020-05-26 15:02:48 -0700

    Assignments 1 through 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
-console.log('Hello from js/scripts.js!');
+jQuery(function() {
+
+  $('#btnWeek1').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_1/', '_blank');
+  });
+
+  $('#btnWeek2').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_2/', '_blank');
+  });
+
+  $('#btnWeek3').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_3/', '_blank');
+  });
+
+  $('#btnWeek4').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_4_assignment/', '_blank');
+  });
+
+  $('#btnWeek5p1').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_5_part_1/', '_blank');
+  });
+
+  $('#btnWeek5p2').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_5_part_2/', '_blank');
+  });
+
+  $('#btnWeek6p1').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_6_part_1/', '_blank');
+  });
+
+  $('#btnWeek6p2').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_6_part_2/', '_blank');
+  });
+
+  $('#btnWeek7').click(function x() {
+    window.open('https://kcsigurdson.com/uclax_wcib_week_7/', '_blank');
+  });
+
+
+});
